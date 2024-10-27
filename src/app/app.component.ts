c56bf162d362e731a7625fdd5a5e5a5ef129b39d
SildelaBarrera <sil.delabarrera@hotmail.com>
2024-10-27 11:16:29 +0100

    Páginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TrucosComponent } from './pages/trucos/trucos.component';
+import { TratosComponent } from './pages/tratos/tratos.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, TrucosComponent, TratosComponent, LoginComponent, RegisterComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,17 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { TratosComponent } from './pages/tratos/tratos.component';
 import { TrucosComponent } from './pages/trucos/trucos.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
     {path:"", redirectTo: "/home", pathMatch: 'full' },
     {path: "home", component: HomeComponent},
     {path: "trucos", component: TrucosComponent},
-    {path:"tratos", component: TratosComponent}
+    {path:"tratos", component: TratosComponent},
+    {path: "login", component: LoginComponent},
+    {path: "register", component: RegisterComponent},
+    {path: "profile", component: ProfileComponent},
+
 ];
